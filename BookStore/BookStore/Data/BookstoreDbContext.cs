79e265b76bf89a573bed8557e7ce58ff062323d7
MylanCoudeville <91263075+MylanCoudeville@users.noreply.github.com>
2023-04-21 20:34:24 +0200

    begin

---
+++ BookStore/BookStore/Data/BookstoreDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class BookstoreDbContext : DbContext
+    {
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options) : base(options) { }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
